1544741792f6f8b18f0f029539ab3000f7d7e164
HenryJrA <henryjrallam7@gmail.com>
2021-09-09 00:53:41 -0400

    CRUD done

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CreateVideo, DetailVideo
+from .views import CreateVideo, DetailVideo, UpdateVideo, DeleteVideo
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
   path('create/', CreateVideo.as_view(), name='video-create'),
-  path('<int:pk>/', DetailVideo.as_view(), name='video-detail') 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  path('<int:pk>/', DetailVideo.as_view(), name='video-detail'),
+  path('<int:pk>/update', UpdateVideo.as_view(), name= 'video-update'),
+  path('<int:pk>/delete', DeleteVideo.as_view(), name='video-delete') 
+] 
